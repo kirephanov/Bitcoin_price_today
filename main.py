3a2a8a376082464a4f0e6bb8db260ae9b11eabe4
Kirill Ephanov <kirill.ephanov@gmail.com>
2023-03-03 14:42:32 +0300

    Bitcoin price added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# Import Flask
 from flask import Flask
 from flask import render_template
 
+# Import for Bitcoin price
+import datetime
+from forex_python.bitcoin import BtcConverter
+
+
 app = Flask(__name__)
+
+b = BtcConverter()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    btc_rub = b.get_latest_price('RUB')
+    btc_usd = b.get_latest_price('USD')
+    btc_eur = b.get_latest_price('EUR')
+
+    return render_template('index.html', btc_rub=btc_rub, btc_usd=btc_usd, btc_eur=btc_eur)
